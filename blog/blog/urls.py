15ea134142a192384bb72b33ee8f455600478364
Ania <anna.lapa@vp.pl>
2017-12-09 15:47:12 +0100

    dodanie templatki

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -1,3 +1,4 @@
+from django.conf.urls import include, url 
 """blog URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
@@ -18,4 +19,5 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    url(r'',include('django_girls.urls')),
+    ]
